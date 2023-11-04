67771c731ce4db44b53ffc3cbe53f9377a3f5b89
VampireNoob <DAYWALKER270581@gmx.de>
2023-11-04 15:55:31 +0100

    Wedding Wish List

---
+++ src/data.js
@@ -0,0 +1,58 @@
+import hotel1 from './hotel.jpg';
+import hotel2 from './hotel 3.jpg';
+import hotel3 from './hotel4.jpg';
+import hotel4 from './hotel1.jpg';
+import spa1 from './spa.jpg';
+import spa2 from './spa2.jpg';
+import spa3 from './spa3.jpg';
+import spa4 from './spa1.jpg';
+import ballon1 from './ballon1.jpg';
+import ballon2 from './ballon.jpg';
+import ballon3 from './ballon3.jpg';
+import ballon4 from './ballon2.jpg';
+import kamera1 from './kamera.jpg';
+import kamera2 from './kamera2.jpg';
+import kamera3 from './kamera3.jpg';
+import kamera4 from './kamera1.jpg';
+import robot1 from './robot.jpg';
+import robot2 from './robot2.jpg';
+import robot3 from './robot3.jpg';
+import robot4 from './robot1.jpg';
+
+export const data = [
+
+    {
+        id: 1,
+        description: 'Wochenende in einem Hotel',
+        price: '400 - 600 Euro',
+        image: [hotel3, hotel2, hotel1, hotel4]
+    },
+
+    {
+        id: 2,
+        description: 'Wellness Gutschein',
+        price: '300 - 400 Euro',
+        image: [spa1, spa2, spa3, spa4]
+    },
+
+    {
+        id: 3,
+        description: 'Heißluftballonfahrt',
+        price: '300 - 600 Euro',
+        image: [ballon1, ballon2, ballon3, ballon4]
+    },
+
+    {
+        id: 4,
+        description: ' Systemkamera',
+        price: '1000 - 3000 Euro',
+        image: [kamera1, kamera2, kamera3, kamera4]
+    },
+
+    {
+        id: 5,
+        description: 'Staubsauger Roboter',
+        price: '500 - 800 Euro',
+        image: [robot1, robot2, robot3, robot4]
+    },
+]
